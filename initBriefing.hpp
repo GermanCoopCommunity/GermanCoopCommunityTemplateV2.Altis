e96c11d03f61beb8db0efd9fa24d2a0baa479741
GeCoQuentin <sajo.adr@gmail.com>
2016-08-07 01:52:25 +0200

    moved in briefing entries
    QTS and Revive Explanation

--- initBriefing.hpp
+++ initBriefing.hpp
@@ -22,3 +22,22 @@ player createDiaryRecord [
 		"Durchführung","Wird von der Führung bestimmt."
 	]
 ];
+
+// QTS entries
+player createDiarySubject ["QTS","QTS"];
+player createDiaryRecord [
+	"QTS",
+	[
+		"Ohrstöpsel",
+			"<br/><font color='#107b1b'>Linke Windows-Taste</font color> auf der Tastatur, um die Ohrstöpsel umzuschalten."
+	]
+];
+// Revive explanation
+player createDiarySubject ["Revive","How To"];
+player createDiaryRecord [
+	"How To",
+	[
+		"How To",
+			"Das Revivesystem ist darauf ausgelegt, den Spieler bei Treffern durch großkalibrige Projektile oder bei Treffern auf ungeschützte Areale zu töten. Nur Sanitäter können wiederbeleben, wenn sie ein Medikit bei sich tragen."
+	]
+];
