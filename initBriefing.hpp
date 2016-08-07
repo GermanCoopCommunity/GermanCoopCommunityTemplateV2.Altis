c36a36c472ec73a760a890b7c77285ad8494225f
GeCoQuentin <sajo.adr@gmail.com>
2016-08-07 02:36:21 +0200

    readded earplugs

--- initBriefing.hpp
+++ initBriefing.hpp
@@ -33,3 +33,14 @@ player createDiaryRecord [
 			"Das Revivesystem ist darauf ausgelegt, den Spieler bei Treffern durch großkalibrige Projektile oder bei Treffern auf ungeschützte Areale zu töten. Nur Sanitäter können wiederbeleben, wenn sie ein Medikit bei sich tragen."
 	]
 ];
+
+
+// QTS entries (must be here, otherwise diary entries aren´t shown in map before mission start)
+player createDiarySubject ["QTS","QTS"];
+player createDiaryRecord [
+	"QTS",
+	[
+		"Ohrstöpsel",
+			"<br/><font color='#107b1b'>Linke Windows-Taste</font color> auf der Tastatur, um die Ohrstöpsel umzuschalten."
+	]
+];
